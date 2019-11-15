3db33d8357ed3fe660cf5e7542df593486c05a2c
MAC <mac@MACs-iMac-2.padavan>
2019-11-15 16:21:07 +0800

    刚学完动态组件

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,46 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import logo from "../viktorImage/test.png"
+import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+    if(scrollTop > 50){
+      this.setState({ hasScrolled: true});
+    }else{
+      this.setState({ hasScrolled: false});
+    }
+  }
+
+  render(){
+    return (
+      <div className={ this.state.hasScrolled ? 'theHeader scrolled' : 'theHeader' }>
+        <div className="theHeaderWrap">
+          <Link to="" className="logo"><img src={require('../viktorImage/test.png')} width="30" /></Link>
+          <Link to="/course">Course</Link>
+          <Link to="/course">Course</Link>
+          <Link to="/course">Course</Link>
+          <Link to="/course">Course</Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,56 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Card from "../components/card"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi Viktor</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+
+    <div className="myBox">
+      <h1>All the courses</h1>
+      <div className="myboxWrap">
+        <Card 
+          title="English Pod"
+          text="It's a really good course."
+          image={require('../viktorImage/bg1@1x.png')}
+        />
+        <Card 
+          title="Chinese Food"
+          text="It's a really good course."
+          image={require('../viktorImage/bg2@1x.png')}
+        />
+        <Card 
+          title="Fantastic"
+          text="It's a really good course."
+          image={require('../viktorImage/bg3@1x.png')}
+        />
+        <Card 
+          title="Twinova Site"
+          text="huo sdfhus sdgw www"
+          image={require('../viktorImage/bg4@1x.png')}
+        />
+      </div>
+    </div>
+
+  
+
+
+
+
+    
+    {/* <div className="logos">
+      <img src={require('../viktorImage/Oval@1x.png')} />
+      <img src={require('../viktorImage/Polygon@1x.png')} />
+      <img src={require('../viktorImage/Star@1x.png')} />
+      <img src={require('../viktorImage/Triangle@1x.png')} />
+    </div> */}
+
+
+    <div className="mainImage" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
